00cf767a581afc5b7044e407226ed11942ac2f1e
Alp Toker <tokeralp@gmail.com>
2022-10-10 19:58:57 +0300

    test1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,25 @@ import first from "../images/first.webp";
 import { useEffect, useState } from "react";
 
 function Home() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    const onPageLoad = () => {
+      setIsLoading(false);
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
   }, []);
 
   return isLoading ? (
