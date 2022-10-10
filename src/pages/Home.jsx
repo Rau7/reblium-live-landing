720c7a148e9af41537ddabe0f83f58982e96237c
Alp Toker <tokeralp@gmail.com>
2022-10-10 20:02:25 +0300

    pageload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,18 +10,9 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    const onPageLoad = () => {
+    window.onload = () => {
       setIsLoading(false);
     };
-
-    // Check if the page has already loaded
-    if (document.readyState === "complete") {
-      onPageLoad();
-    } else {
-      window.addEventListener("load", onPageLoad);
-      // Remove the event listener when component unmounts
-      return () => window.removeEventListener("load", onPageLoad);
-    }
   }, []);
 
   return isLoading ? (
